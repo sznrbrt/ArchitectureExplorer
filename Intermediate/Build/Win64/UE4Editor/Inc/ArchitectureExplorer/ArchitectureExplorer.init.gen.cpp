0d160eec0ce9b3b8a15af2c472a18a539ccc818c
Norbert Szűcs <szucs.norbert@outlook.com>
2021-01-02 00:53:18 -0500

    feat: add camera ofsset

--- Intermediate/Build/Win64/UE4Editor/Inc/ArchitectureExplorer/ArchitectureExplorer.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ArchitectureExplorer/ArchitectureExplorer.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeArchitectureExplorer_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x05B1C19D,
-				0xB4D05979,
+				0xB4D62AFE,
+				0xF1E4AB5B,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
